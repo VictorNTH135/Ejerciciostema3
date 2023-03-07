ae4f02778c2c13b6d4d6e2a91c85f59c80839ccc
Victornth135 <victorquick135@gmail.com>
2023-03-06 23:30:07 -0600

    Esta es mi prueba de funciones del curso de Introducción a la programación

---
+++ ejerciciot3.java
@@ -0,0 +1,18 @@
+//Primera parte:Hacer una función con 3 parametros que se sumen entre sí & llamar la función para darle valores
+public class ejerciciot3 {
+
+    public static int suma3nu(int nu1, int nu2, int nu3) {
+        int suma = nu1 + nu2 + nu3;
+        return suma;
+    }
+
+    public static void main(String[] args) {
+        int resultado = suma3nu(67, 54, 21);
+        System.out.println(resultado);
+    }
+}
+
+
+
+
+
